1fa8fb1ebb855c618aeddb2694f0767c06cc47c7
Josh Cowen <josh.cowen@icloud.com>
2026-03-17 19:52:08 +0000

    Fix: correct nvim-treesitter config to install correct plugin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,3 +7,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.wo.relativenumber = true
 
+-- recognise terraform files, set filetypes
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+
