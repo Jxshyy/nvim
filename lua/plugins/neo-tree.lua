03bc50be6c022a60d3ba96dceb0e5e851bec9f5b
Josh Cowen <josh.cowen@icloud.com>
2026-03-18 20:31:20 +0000

    Update: show hidden files by default

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,17 +1,23 @@
-return{
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-      require("neo-tree").setup({
-        close_if_last_window = true,
-      })
-    end,
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false -- neo-tree will lazily load itself
-  }
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_gitignored = true,
+          }
+				}
+			})
+		end,
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+	},
 }
